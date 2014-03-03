dda99b6c684d236a4d794faa5a0e6e986f313e46
nitindandriyal <nitin.dandriyal@gmail.com>
2014-03-04 07:51:40 +0800

    Added Open sanf font

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <meta charset="utf-8" />
 <meta name="language" content="en" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no">
+<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:300,400,600"/>
 <link rel="stylesheet" type="text/css" href="css/main.css" />
 <link rel="stylesheet" type="text/css" href="css/googleapi_style.css" />
 <link rel="stylesheet" type="text/css" href="css/dr_style.css" />
