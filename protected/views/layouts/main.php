6c99bf80689b54ab974bf1a1c44715bb3a21ce61
nitindandriyal <nitin.dandriyal@gmail.com>
2014-03-22 04:07:07 +0800

    CHanges for page header

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -5,70 +5,45 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta charset="utf-8" />
 <meta name="language" content="en" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no">
-<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:300,400,600"/>
-<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
-<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/dr_style.css" />
-<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/normalize.css" />
-<title>DReview</title>
-<script type="text/javascript">
-    (function () {
-        var win = $(window), timeout, onload, trigger;
+<meta name="viewport"
+	content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no">
+	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:300,400,600" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/dr_style.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/normalize.css" />
+	<title>DReview</title>
 
-        trigger = function () {
-            win.trigger('rs_load');
-            window.rs_loaded = true;
-        };
-
-        onload = function () {
-            clearTimeout(timeout);
-            trigger();
-        };
-
-        timeout = setTimeout(function () {
-            win.off('load', onload);
-            trigger();
-        }, 800);
-
-        win.on('load', onload);
-    })();
-</script>
-
-<meta name="fb:admins" content="303803, 10700183" />
-<meta name="rs:page-format" content="landing" />
-<script>window.dataLayer = window.dataLayer || [];</script>
 </head>
 <body>
-<div id="PageTop">
-	<nav>
-		<ul>
-			<li>
-				<img src="../images/logo.jpg" class="logo"/>
-			</li>
-			<li>
-				<a href="index">Home </a>
-			</li>
-			<li><a href="/find"	data-gtm='{"event":"navlink:find-a-doctor","state":"navlink","action":"click","widget":"nav"}'>Find a Doctor</a>
-			</li>
-			<li><a href="/review/create" 
-						data-gtm='{"event":"navlink:write-a-review","state":"navlink","action":"click","widget":"nav"}'>Write a Review
-					</a>
-			</li>
-			</ul>
+	<div id="PageTop">
+	    <div class="logo">
+	     	<img src="../images/logo.jpg"/>
+	     </div>
+		<nav>
 			<div class="actions">
-				<a href="login" rel="auth-require" data-tab="signup">Sign In</a>
-			</div>		
-	</nav>
-</div>
- <?php echo $content; ?>
- <div class="clear"></div>
+				<ul>
+					<li><a href="index">Home </a>
+					</li>
+					<li><a href="find">Find a Doctor</a>
+					</li>
+					<li><a href="writereview">Write a Review</a>
+					</li>
+					<li><a href="login" rel="auth-require" data-tab="signup">Sign In</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</div>
+	<?php echo $content; ?>
+	<div class="clear"></div>
 
 	<div id="footer">
 		<div id="footer-text">
 			<div align="center">
 				<center>
 					<p>&copy; 2014 DReview, Inc. All rights are reserved.
+				
 				</center>
 			</div>
 		</div>
