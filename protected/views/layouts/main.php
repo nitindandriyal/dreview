5c39b0ab08e8c2f9bc6e93ef48d295f978e24abb
nitindandriyal <nitin.dandriyal@gmail.com>
2014-03-22 12:34:35 +0800

    modified footer

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -7,47 +7,96 @@
 <meta name="language" content="en" />
 <meta name="viewport"
 	content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no">
-	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:300,400,600" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/dr_style.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/normalize.css" />
+	<link rel="stylesheet"
+		href="//fonts.googleapis.com/css?family=Open+Sans:300,400,600" />
+	<link rel="stylesheet" type="text/css"
+		href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
+	<link rel="stylesheet" type="text/css"
+		href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+	<link rel="stylesheet" type="text/css"
+		href="<?php echo Yii::app()->request->baseUrl; ?>/css/dr_style.css" />
+	<link rel="stylesheet" type="text/css"
+		href="<?php echo Yii::app()->request->baseUrl; ?>/css/normalize.css" />
 	<title>DReview</title>
 
 </head>
 <body>
 	<div id="PageTop">
-	    <div class="logo">
-	     	<img src="../images/logo.jpg"/>
-	     </div>
+		<div class="logo">
+			<img src="../images/logo.jpg" />
+		</div>
 		<nav>
-			<div class="actions">
-				<ul>
-					<li><a href="index">Home </a>
-					</li>
-					<li><a href="find">Find a Doctor</a>
-					</li>
-					<li><a href="writereview">Write a Review</a>
-					</li>
-					<li><a href="login" rel="auth-require" data-tab="signup">Sign In</a>
-					</li>
-				</ul>
-			</div>
+		<div class="actions">
+			<ul>
+				<li><a href="index">Home </a>
+				</li>
+				<li><a href="find">Find a Doctor</a>
+				</li>
+				<li><a href="writereview">Write a Review</a>
+				</li>
+				<li><a href="login" rel="auth-require" data-tab="signup">Sign In</a>
+				</li>
+			</ul>
+		</div>
 		</nav>
 	</div>
 	<?php echo $content; ?>
-	<div class="clear"></div>
+	<footer class="site-footer">
+		<div class="row" style="margin-left: 50px">
+			<ul class="footer-links">
+				<li class="foot-link_item">
+					<h3>About</h3>
+				</li>
+				<li class="foot-link_item"><a href="/about-us/">About us</a>
+				</li>
+				<li class="foot-link_item"><a href="/advertising">Advertise</a>
+				</li>
+				<li class="foot-link_item"><a href="/legals">Legals</a>
+				</li>
+				<li class="foot-link_item"><a href="mailto:feedback@dreview.com">Feedback</a>
+				</li>
+			</ul>
+			<ul class="footer-links">
+				<li class="foot-link_item">
+					<h3>Our Sites</h3>
+				</li>
+				<li class="foot-link_item"><a href="http://www.learnable.com"
+					target="_blank">Learnable</a>
+				</li>
+				<li class="foot-link_item"><a href="http://reference.dreview.com"
+					target="_blank">Reference</a>
+				</li>
+				<li class="foot-link_item"><a href="/hosting-reviews/"
+					target="_blank">Hosting Reviews</a>
+				</li>
+				<li class="foot-link_item"><a href="/web-foundations/">Web
+						Foundations</a>
+				</li>
+			</ul>
 
-	<div id="footer">
-		<div id="footer-text">
-			<div align="center">
-				<center>
-					<p>&copy; 2014 DReview, Inc. All rights are reserved.
-				
-				</center>
-			</div>
+			<ul class="footer-links">
+				<li class="foot-link_item">
+					<h3>Connect</h3>
+				</li>
+				<li class="foot-link_item foot-link_item--icons"><a href="/feed"><i
+						class="icon-rss icon-blocks icon-blocks--rss"></i> </a> <a
+					href="/newsletter"><i
+						class="icon-envelope-alt icon-blocks icon-blocks--newsletter"></i>
+				</a> <a href="https://www.facebook.com/dreview" target="_blank"><i
+						class="icon-facebook icon-blocks icon-blocks--facebook"></i> </a>
+					<a href="http://twitter.com/sitepointdotcom" target="_blank"><i
+						class="icon-twitter icon-blocks icon-blocks--twitter"></i> </a> <a
+					href="https://plus.google.com/+dreview" target="_blank"><i
+						class="icon-google-plus icon-blocks icon-blocks--google-plus"></i>
+				</a>
+				</li>
+			</ul>
+			<p class="site-footer_copyright" style="margin-right: 10px">
+          &copy; 2000 &ndash; 2014 DReview, Inc. All rights are reserved.
+        	</p>
 		</div>
-	</div>
+	
+	</footer>
 
 </body>
 </html>
