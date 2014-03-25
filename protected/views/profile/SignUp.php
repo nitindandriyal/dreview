3c22416d7a6f453a3748c32029f29c1f68faf8f9
nitindandriyal <nitin.dandriyal@gmail.com>
2014-03-25 23:18:18 +0800

    user input validations

--- protected/views/profile/SignUp.php
+++ protected/views/profile/SignUp.php
@@ -3,36 +3,69 @@
 		<img src="../images/signup.jpg" style="max-width:100%; height:100%;border-radius: 58px;"/>
 	</div> 
 	<div id="SignInDiv">		
-		<?php echo CHtml::beginForm(); ?>
-		
+		<?php echo CHtml::beginForm() ?>
+
 		    <span class="loginTblSpan">SignUp</span>
 		    <span class="loginTblSpan" id="CreateAccount">Create a new account</span>
-		    
-		    <?php echo CHtml::errorSummary($model); ?>
 		 
 		    <div class="row">
 		        <?php echo CHtml::label('First Name', ''); ?><br>
-		        <?php echo CHtml::activeTextField($model, 'firstname') ?>
+		        <?php echo CHtml::activeTextField($model, 'firstname'); 
+		        if(!empty($model->getErrors('firstname'))){
+		        
+		        	$err = $model->getErrors('firstname');
+		        		
+		        	?>
+		        <span class="bubble">
+		        <?php echo $err[0] ?></span><?php }?>		        
 		    </div><br>
 		 
 		    <div class="row">
 		        <?php echo CHtml::label('Last Name', ''); ?><br>
-		        <?php echo CHtml::activeTextField($model, 'lastname') ?>
+		        <?php echo CHtml::activeTextField($model, 'lastname');
+		        if(!empty($model->getErrors('lastname'))){
+	
+					$err = $model->getErrors('lastname');
+					
+		        ?>
+		        <span class="bubble"><?php echo $err[0] ?></span>
+		        <?php }?>
 		    </div><br>
 		    
 		    <div class="row">
 		        <?php echo CHtml::activeLabel($model,'Email'); ?><br>
-		        <?php echo CHtml::activeTextField($model,'email') ?>
+		        <?php echo CHtml::activeTextField($model,'email'); 
+		        if(!empty($model->getErrors('email'))){
+		        
+		        	$err = $model->getErrors('email');
+		        		
+		        	?>
+		        <span class="bubble">
+		        <?php echo $err[0] ?></span><?php }?>	
 		    </div><br>
 		    
 		    <div class="row">
 		        <?php echo CHtml::activeLabel($model,'Password'); ?><br>
-		        <?php echo CHtml::activePasswordField($model,'password') ?>
+		        <?php echo CHtml::activePasswordField($model,'password'); 
+		        if(!empty($model->getErrors('password'))){
+		        
+		        	$err = $model->getErrors('password');
+		        		
+		        	?>
+		        <span class="bubble">
+		        <?php echo $err[0] ?></span><?php }?>
 		    </div><br>		    		    
 		 
 		    <div class="row">
 		        <?php echo CHtml::label('Confirm Password', ''); ?><br>
-		        <?php echo CHtml::activePasswordField($model, 'confirmPassword') ?>
+		        <?php echo CHtml::activePasswordField($model, 'confirmPassword'); 
+		        if(!empty($model->getErrors('confirmPassword'))){
+		        
+		        	$err = $model->getErrors('confirmPassword');
+		        		
+		        	?>
+		        <span class="bubble">
+		        <?php echo $err[0] ?></span><?php }?>
 		    </div><br>
 		    		 
 		    <div class="row submit">
