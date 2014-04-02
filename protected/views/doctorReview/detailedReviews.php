b4fbd2478283b91249715bc2354c22ec90561f4a
Anurag <anurag786@gmail.com>
2014-04-02 22:30:37 +0530

    changes done to display doctor's full details

--- protected/views/doctorReview/detailedReviews.php
+++ protected/views/doctorReview/detailedReviews.php
@@ -9,11 +9,53 @@
 <div class="container">
 
 </div>
-<?php echo "hello";?>
+
 <?php foreach ($docDetail as $row){?>
-<h2><?php echo $row['FIRST_NAME'] ;echo "hello";?>  
+<h2><?php echo "Dr. " ;echo $row['FIRST_NAME'];echo " " ;echo $row['LAST_NAME'] ;?>  
 </h2>
+<br>
+<?php echo "DOB: ";echo $row['DOB']; ?>
+<br>
+<?php echo "LANG_SPOKEN: ";echo $row['LANG_SPOKEN']; ?>
+<br>
+<?php echo "PRACTICE_ST_DT: ";echo $row['PRACTICE_ST_DT']; ?>
+<br>
+<?php echo "USER_RATING: ";echo $row['USER_RATING']; ?>
+<br>
+<?php echo "MEDICINE: ";echo $row['MEDICINE']; ?>
+<br>
+<?php echo "GENDER: ";echo $row['GENDER']; ?>
+<br>
+<?php echo "TOTAL_REVIEWS: ";echo $row['TOTAL_REVIEWS']; ?>
+<br>
 <?php }?>
+
+<?php foreach ($docQualification as $row){?>
+<br>
+<?php echo "QUALIFICATION: ";echo $row['QUALIFICATION']; ?>
+<br>
+<?php echo "INSTITUTION: ";echo $row['INSTITUTION']; ?>
+<br>
+<?php echo "SUPER_SPECIALITY: ";echo $row['SUPER_SPECIALITY']; ?>
+<br>
+<?php echo "SUB_SPECIALITY: ";echo $row['SUB_SPECIALITY']; ?>
+
+<?php }?>
+
+<?php foreach ($docAppointmentDetails as $row){?>
+<br>
+<?php echo "Hospital: ";echo $row['OFFICE_NAME']; ?>
+<br>
+<?php echo "PRI_CONTACT_NO: ";echo $row['PRI_CONTACT_NO']; ?>
+<br>
+<?php echo "EMAIL: ";echo $row['EMAIL']; ?>
+<br>
+<?php echo "DISTRICT: ";echo $row['DISTRICT']; ?>
+<br>
+<?php echo "AREA: ";echo $row['AREA']; ?>
+
+<?php }?>
+
 <p>
 
 
