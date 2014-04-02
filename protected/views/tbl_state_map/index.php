90ccd18b7fda94647d722c005d3fc7a3b1e2fdf1
Anurag <anurag786@gmail.com>
2014-04-02 21:46:25 +0530

    mod

--- protected/views/tbl_state_map/index.php
+++ protected/views/tbl_state_map/index.php
@@ -10,30 +10,35 @@ $this->breadcrumbs=array(
 		echo ' Doctors';?></h3>
 </div>
 <p>
-
+<?php $columnCnt =0;?>
 <div class="container">
 <br>
 <div id="all-local-landing-pages" class="widget-item-list row">	
             <?php for($stateCount=0;$stateCount<count($state);$stateCount++) {?>
-            	<?php if($stateCount%4==0) {?>
+            	<?php if($columnCnt%4==0) {?>
             	</div>	
             	<div class="container">
                 <br>
              <?php } ?>
+             <?php if(!empty($city[$stateCount][0])){ $columnCnt++;?>
+             
 			<div class="col-sm-3">
 				<ul class='list-unstyled list-spaceless'>
-					<li><h4><?php echo CHtml::link($state[$stateCount],array('doctor/DocSearchByState','param1'=>$state[$stateCount],'param2'=>$firstParam)); ?></h4></li>
+					<li><h2><?php echo CHtml::link($state[$stateCount],array('doctor/DocSearchByState','param1'=>$state[$stateCount],'param2'=>$firstParam)); ?></h2></li>
 					<?php for($cityCount=0;$cityCount<count($city[$stateCount]);$cityCount++) {?>
 				<li><?php echo CHtml::link($city[$stateCount][$cityCount],array('doctor/DocSearchByCity','param1'=>$city[$stateCount][$cityCount],'param2'=>$firstParam)); ?></li>
 				
 					<?php  } ?>
 					
 				</ul>
+			 <?php } ?>
+				
 			</div>
+			 <?php } ?>
 			
-           <?php } ?>
          
    </div>
+   
 </div>
 	
 </p>
