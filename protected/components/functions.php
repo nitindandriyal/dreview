3cc74f87b3fb7ab9488e3a03f5158f090f49d7bf
nitindandriyal <nitin.dandriyal@gmail.com>
2014-04-02 20:57:27 +0800

    moved functions.php from config to components

---
+++ protected/components/functions.php
@@ -0,0 +1,49 @@
+<?php
+
+require 'protected/config/dbconfig.php';
+
+class User 
+{
+
+    function checkFBUser($uid, $oauth_provider, $username,$email,$twitter_otoken,$twitter_otoken_secret) 
+	{
+	
+		$fetchQuery = "SELECT username, email, oauth_provider, oauth_uid FROM `tbl_users` WHERE oauth_uid = '$uid' and oauth_provider = '$oauth_provider'";		
+        $query = mysql_query($fetchQuery ) or die(mysql_error());
+        $result = mysql_fetch_array($query);
+        
+        if (empty($result))
+        {
+            #user not present. Insert a new Record
+            $query = mysql_query("INSERT INTO `tbl_users` (oauth_provider, oauth_uid, username,email) VALUES ('$oauth_provider', $uid, '$username','$email')") or die(mysql_error());
+            $query = mysql_query($fetchQuery );
+            $result = mysql_fetch_array($query );            
+        }
+       
+        return $result;
+    } 
+
+    function checkGoogleUser($oauth_provider, $username,$email)
+    {
+
+		$fetchQuery = "SELECT username, email, oauth_provider FROM `tbl_users` WHERE email = '$email' and oauth_provider = '$oauth_provider'";		
+        $query = mysql_query($fetchQuery ) or die(mysql_error());
+        $result = mysql_fetch_array($query);
+        
+        if (empty($result))
+        {
+            #user not present. Insert a new Record
+            $query = mysql_query("INSERT INTO `tbl_users` (oauth_provider, username, email, status) VALUES ('$oauth_provider', '$username', '$email', 'ACTIVE')") or die(mysql_error());
+            $query = mysql_query($fetchQuery );
+            $result = mysql_fetch_array($query );            
+        }
+       
+        return $result;
+       
+    }
+    
+    
+
+}
+
+?>
